b92675c18dd09602f30ee81e1f047f29917d220b
RootPHG <phg0517@gmail.com>
2022-10-06 21:27:27 +0900

    이미지 테스트 세팅

---
+++ unit_image_test.py
@@ -0,0 +1,53 @@
+from pico2d import *
+
+def handle_events():
+    global running
+    global dir_x
+    global dir_y
+    global anime
+
+    events = get_events()
+    for event in events:
+        if event.type == SDL_QUIT:
+            running = False
+        elif event.type == SDL_KEYDOWN:
+            if event.key == SDLK_RIGHT:
+                dir_x += 1
+                anime = 1
+            elif event.key == SDLK_LEFT:
+                dir_x -= 1
+                anime = 0
+            elif event.key == SDLK_UP:
+                dir_y += 1
+                anime -= 2
+            elif event.key == SDLK_DOWN:
+                dir_y -= 1
+                anime -= 2
+            elif event.key == SDLK_ESCAPE:
+                running = False
+        elif event.type == SDL_KEYUP:
+            if event.key == SDLK_RIGHT:
+                dir_x -= 1
+                anime = 3
+            elif event.key == SDLK_LEFT:
+                dir_x += 1
+                anime = 2
+            elif event.key == SDLK_UP:
+                dir_y -= 1
+                anime += 2
+            elif event.key == SDLK_DOWN:
+                dir_y += 1
+                anime += 2
+
+
+open_canvas()
+
+main_char1 = load_image('doom_character1.png')
+main_char2 = load_image('doom_pistol.png')
+main_char3 = load_image('character_type_swat.png')
+main_char4 = load_image('main_character.png')
+mod_char = load_image('clasic_monster.png')
+
+
+
+
