da6495c42c9372d082ca1454cae236a7a8980be4
RootPHG <phg0517@gmail.com>
2022-10-09 05:51:22 +0900

    주인공 캐릭터 기본 이동

--- unit_image_test.py
+++ unit_image_test.py
@@ -13,36 +13,50 @@ def handle_events():
         elif event.type == SDL_KEYDOWN:
             if event.key == SDLK_RIGHT:
                 dir_x += 1
-                anime = 1
             elif event.key == SDLK_LEFT:
                 dir_x -= 1
-                anime = 0
             elif event.key == SDLK_UP:
                 dir_y += 1
-                anime -= 2
             elif event.key == SDLK_DOWN:
                 dir_y -= 1
-                anime -= 2
             elif event.key == SDLK_ESCAPE:
                 running = False
         elif event.type == SDL_KEYUP:
             if event.key == SDLK_RIGHT:
                 dir_x -= 1
-                anime = 3
             elif event.key == SDLK_LEFT:
                 dir_x += 1
-                anime = 2
             elif event.key == SDLK_UP:
                 dir_y -= 1
-                anime += 2
             elif event.key == SDLK_DOWN:
                 dir_y += 1
-                anime += 2
 
+def head_dir():
+    global dir_x
+    global dir_y
+    global anime
+
+    # (캐릭터가 바라보는 방향)
+    if dir_x < 0 and dir_y < 0:
+        anime = 0
+    elif dir_x < 0 and dir_y == 0:
+        anime = 1
+    elif dir_x < 0 and dir_y > 0:
+        anime = 2
+    elif dir_x == 0 and dir_y > 0:
+        anime = 3
+    elif dir_x > 0 and dir_y > 0:
+        anime = 4
+    elif dir_x > 0 and dir_y == 0:
+        anime = 5
+    elif dir_x > 0 and dir_y < 0:
+        anime = 6
+    elif dir_x == 0 and dir_y < 0:
+        anime = 7
 
 open_canvas()
 
-main_char1 = load_image('hunter_sprite.png')
+main_char2 = load_image('ranger_sprite.png')
 
 running = True
 frame = 0
@@ -50,14 +64,19 @@ dir_x = 0
 dir_y = 0
 x = 800 // 2
 y = 600 // 2
-anime = 7
+anime = 0
 
 while running:
     clear_canvas()
-    main_char1.clip_draw(frame * 59 + 220, anime * 90, 55, 90, x, y)
+    main_char2.clip_draw(frame * 62 + 200, anime * 79 + 15, 60, 84, x, y)
     update_canvas()
-    frame = (frame + 1) % 8
-    delay(0.3)
+    # 멈췄을경우 이미지 고정
+    if dir_x == 0 and dir_y == 0:
+        frame = 3
+    else :
+        frame = (frame + 1) % 8
+    delay(0.05)
+    head_dir()
     x += dir_x * 5
     y += dir_y * 5
     handle_events()
