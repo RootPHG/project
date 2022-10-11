4dd35b660dbac6ff92d09ac9eae16015d9a2fbe1
RootPHG <phg0517@gmail.com>
2022-10-12 03:18:03 +0900

    Update upgrade_test.py

---
+++ upgrade_test.py
@@ -0,0 +1,34 @@
+from pico2d import *
+
+class Ranger:
+    pass
+
+class Monster:
+    pass
+
+
+
+
+
+
+
+
+
+
+# initialization code
+open_canvas()
+running = True
+
+# game main loop code
+while running:
+    handle_events()
+
+
+    clear_canvas()
+
+    update_canvas()
+
+    delay(0.04)
+
+# finalization code
+close_canvas()
