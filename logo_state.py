0ba5ac5e9b9be0395c2a069d3f4baedffa02040b
RootPHG <phg0517@gmail.com>
2022-10-16 22:33:07 +0900

    코드 정리

--- logo_state.py
+++ logo_state.py
@@ -1,11 +1,14 @@
+import game_framework
 from pico2d import *
 
-running = True
+import play_state
+
 image = None
 logo_time = 0.0
 
 def enter():
     global image
+    image = load_image('TUK_logo.jpg')
     pass
 
 def exit():
@@ -15,19 +18,21 @@ def exit():
 
 def update():
     global logo_time
-    global running
     if logo_time > 0.5:
-        running = False
+        logo_time = 0
+        game_framework.change_state(play_state)
     delay(0.01)
     logo_time += 0.01
     pass
 
 def draw():
     clear_canvas()
-
+    image.draw(400,300)
     update_canvas()
     pass
 
 def handle_events():
     events = get_events()
     pass
+
+
